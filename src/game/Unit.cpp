0f711c2faaf5f333552a0ceec4fa7969ddc1c8b0
Neltor <ChrEinz@web.de>
2011-10-05 01:11:43 +0200

    pala judgement of crusader fix 2.0

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -5388,48 +5388,62 @@ uint32 Unit::SpellDamageBonusTaken(Unit *pCaster, SpellEntry const *spellProto, 
 	int32 saveAuraMod = 0;	
 	if (schoolMask & SPELL_SCHOOL_MASK_HOLY) {                          //Judgement of the Crusader	
     A = GetAura(SPELL_AURA_MOD_DAMAGE_TAKEN, SPELLFAMILY_PALADIN, 536870912);	
-    if (A) {	
-    saveAuraMod = A->GetModifier()->m_amount;	
-    switch (spellProto->SpellIconID) {	
-      case 561:                                                     	
-        if (spellProto->SpellFamilyFlags.Flags == 33554432)       //Seal of Command proc: 21% of maximum applied	
-          A->GetModifier()->m_amount *= 0.21;	
-        else                            //Judgement of Command: 43% of maximum applied	
-          A->GetModifier()->m_amount *= 0.43;	
-        break;	
-      case 25:                                                      	
-        if (spellProto->SpellFamilyFlags.Flags == 134217728)      //Seal of Righteousness proc: 10% of maximum applied	
-          A->GetModifier()->m_amount *= 0.10;	
-        else                                //Judgement of Righteousness: 59% of maximum applied	
-          A->GetModifier()->m_amount *= 0.59;	
-        break;	
-      case 292:                              //Exorcism: 43% of maximum applied	
-        A->GetModifier()->m_amount *= 0.43;	
-        break;	
-      case 156:                              //Holy Shock: 43% of maximum applied	
-        A->GetModifier()->m_amount *= 0.43;	
-        break;	
-      case 51:                            //Consecration: 33% of maximum applied	
-        A->GetModifier()->m_amount *= 0.33;	
-        break;	
-      case 158:                                //Holy Wrath: 19% of maximum applied	
-        A->GetModifier()->m_amount *= 0.19;	
-        break;	
-      default:	
-        break;
-    }	
+    if (A && A->GetModifier()) {	
+      saveAuraMod = A->GetModifier()->m_amount;	
+      A->GetModifier()->m_amount = 0;	
     }	
   }
 
     int32 TakenAdvertisedBenefit = SpellBaseDamageBonusTaken(GetSpellSchoolMask(spellProto));
 
-	if (A)	
-      A->GetModifier()->m_amount = saveAuraMod;
-
     // apply benefit affected by spell power implicit coeffs and spell level penalties
     TakenTotal = SpellBonusWithCoeffs(spellProto, TakenTotal, TakenAdvertisedBenefit, 0, damagetype, false);
 
     float tmpDamage = (int32(pdamage) + TakenTotal * int32(stack)) * TakenTotalMod;
+    if (A && A->GetModifier()) //Apply Judgement of the Crusader bonus after other calculations are done	
+	{	
+	   A->GetModifier()->m_amount = saveAuraMod;	
+	   float coeff = 0.0f;	
+	switch (spellProto->SpellIconID) 	
+    {	
+      case 25:    	
+        if (spellProto->SpellVisual == 5622)    //Seal of Righteousness proc: 10% of maximum applied	
+          coeff = 0.1f;	
+        else                    //Judgement of Righteousness: 50% of maximum applied	
+          coeff = 0.5f;	
+        break;
+      case 292:                    //Exorcism: 43% of maximum applied	
+      case 302:                    //Hammer of Wrath: 43% of maximum applied	
+        coeff = 0.43f;	
+        break;	
+      case 156:                             	
+        if (spellProto->SpellVisual == 3400)
+        {
+          if (damagetype == DOT)        //Holy Fire DoT: 26% of maximum applied
+          coeff = 0.26f;	
+          else                  //Holy Fire Initial Damage: 32% of maximum applied  	
+          coeff = 0.32f;
+        }
+        else                    //Holy Shock: 43% of maximum applied	
+        coeff = 0.43f;	
+        break;	
+      case 51:                    //Consecration: 33% of maximum applied	
+        coeff = 0.33f;
+        break;	
+      case 158:                    //Holy Wrath: 19% of maximum applied
+        coeff = 0.19f;	
+        break;
+      case 237:                    //Smite: 32% of maximum applied	
+      case 1874:                  //Holy Nova: 32% of maximum applied
+        coeff = 0.32f;
+        break;
+      default:	
+       break;	
+    }
+    if (coeff > 0)
+      tmpDamage += tmpDamage*coeff > saveAuraMod ? saveAuraMod : tmpDamage*coeff;	
+  }
+
 
     return tmpDamage > 0 ? uint32(tmpDamage) : 0;
 }
@@ -6036,6 +6050,17 @@ uint32 Unit::MeleeDamageBonusTaken(Unit *pCaster, uint32 pdamage,WeaponAttackTyp
         TakenFlat += GetTotalAuraModifier(SPELL_AURA_MOD_MELEE_DAMAGE_TAKEN);
 
     // ..taken flat (by school mask)
+    // Check if victim auras must be modified depending on the spell
+	
+  Aura* A = 0;     
+  int32 saveAuraMod = 0;	
+  if (schoolMask & SPELL_SCHOOL_MASK_HOLY) {                          //Judgement of the Crusader	
+    A = GetAura(SPELL_AURA_MOD_DAMAGE_TAKEN, SPELLFAMILY_PALADIN, 536870912);	
+    if (A && A->GetModifier()) {	
+      saveAuraMod = A->GetModifier()->m_amount;	
+      A->GetModifier()->m_amount = 0;	
+    }	
+  }
     TakenFlat += GetTotalAuraModifierByMiscMask(SPELL_AURA_MOD_DAMAGE_TAKEN, schoolMask);
 
     // PERCENT damage auras
@@ -6062,6 +6087,19 @@ uint32 Unit::MeleeDamageBonusTaken(Unit *pCaster, uint32 pdamage,WeaponAttackTyp
     }
 
     float tmpDamage = float(int32(pdamage) + TakenFlat * int32(stack)) * TakenPercent;
+
+  if (A && A->GetModifier())  //Apply Judgement of the Crusader bonus after other calculations are done	
+  {	
+    A->GetModifier()->m_amount = saveAuraMod;	
+    if (spellProto->SpellIconID == 561) 	
+    {       
+      if (spellProto->SpellVisual == 5622)          //Seal of Command proc: 29% of maximum applied	
+        tmpDamage += tmpDamage*0.29 > saveAuraMod ? saveAuraMod : tmpDamage*0.29;	
+      else                          //Judgement of Command: 43% of maximum applied	
+        tmpDamage += tmpDamage*0.43 > saveAuraMod ? saveAuraMod : tmpDamage*0.43;	
+    }	
+  }	
+
 
     // bonus result can be negative
     return tmpDamage > 0 ? uint32(tmpDamage) : 0;
