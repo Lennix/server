b17cd44d8123d9097b5b56adcb84199c048a65dc
Neltor <ChrEinz@web.de>
2011-10-05 01:24:49 +0200

    bugfix if used fear or sheep

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7043,11 +7043,13 @@ void Unit::TauntApply(Unit* taunter)
 
     if (target && target == taunter)
         return;
+	if (!HasAuraType(SPELL_AURA_MOD_FEAR) && !HasAuraType(SPELL_AURA_MOD_CONFUSE))	
+	{
+		SetInFront(taunter);
 
-    SetInFront(taunter);
-
-    if (((Creature*)this)->AI())
-        ((Creature*)this)->AI()->AttackStart(taunter);
+		if (((Creature*)this)->AI())
+			((Creature*)this)->AI()->AttackStart(taunter);
+	}
 
     m_ThreatManager.tauntApply(taunter);
 }
@@ -7083,7 +7085,7 @@ void Unit::TauntFadeOut(Unit *taunter)
     m_ThreatManager.tauntFadeOut(taunter);
     target = m_ThreatManager.getHostileTarget();
 
-    if (target && target != taunter)
+    if (target && target != taunter && !HasAuraType(SPELL_AURA_MOD_FEAR) && !HasAuraType(SPELL_AURA_MOD_CONFUSE))
     {
         SetInFront(target);
 
@@ -7145,8 +7147,9 @@ bool Unit::SelectHostileTarget()
         target = m_ThreatManager.getHostileTarget();
 
     if (target)
-    {
-        if (!hasUnitState(UNIT_STAT_STUNNED | UNIT_STAT_DIED))
+    {        
+        if (!hasUnitState(UNIT_STAT_STUNNED | UNIT_STAT_DIED)	
+		&& !HasAuraType(SPELL_AURA_MOD_FEAR) && !HasAuraType(SPELL_AURA_MOD_CONFUSE)) //Prevent unit from attacking while feared/confused 
         {
             SetInFront(target);
             ((Creature*)this)->AI()->AttackStart(target);
